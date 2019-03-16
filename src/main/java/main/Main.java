ddf39bf64db09a116b4ba71fe2a73b1561d4ae43
Natrix31 <uzhvlad@yandex.ru>
2019-03-16 14:55:25 +0300

    portion of changes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,17 +1,22 @@
+package main;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.Frontend;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Frontend frontend = new Frontend();
 
-        Server server = new Server(8080);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(frontend),"/mirror");
+
+        Server server = new Server(8080);
         server.setHandler(context);
-        context.addServlet(new ServletHolder(frontend),"/autform");
 
         server.start();
+        System.out.println("Server started");
         server.join();
     }
 }
