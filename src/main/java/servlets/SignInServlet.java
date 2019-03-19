68babacea0f96a0c7e29fc86f3748d9fa4ac03cd
Natrix31 <uzhvlad@yandex.ru>
2019-03-19 08:24:34 +0300

    added status output

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -3,9 +3,11 @@ package servlets;
 import accounts.AccountService;
 import accounts.UserProfile;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SignInServlet extends HttpServlet {
   private final AccountService accountService;
@@ -15,7 +17,7 @@ public class SignInServlet extends HttpServlet {
   }
 
   public void doPost(HttpServletRequest request,
-                     HttpServletResponse response){
+                     HttpServletResponse response) throws ServletException, IOException {
     String login = request.getParameter("login");
     String pass = request.getParameter("password");
 
@@ -29,11 +31,13 @@ public class SignInServlet extends HttpServlet {
     if (profile == null || !profile.getPass().equals(pass)){
       response.setContentType("text/html;charset=utf-8");
       response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      response.getWriter().println("Unauthorized");
       return;
     }
 
     accountService.addSession(request.getSession().getId(), profile);
     response.setContentType("text/html;charset=utf-8");
     response.setStatus(HttpServletResponse.SC_OK);
+    response.getWriter().println("Authorized: "+login);
   }
 }
